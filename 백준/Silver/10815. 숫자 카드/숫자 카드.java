4a7016b0444d55a2e0bf04e4e8dbac4e7a4d76e1
changyu13 <102674319+changyu13@users.noreply.github.com>
2025-03-09 11:28:17 +0900

    [Silver V] Title: 숫자 카드, Time: 3168 ms, Memory: 177392 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,55 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Arrays;
-import java.io.*;
+import java.util.HashMap;
 
 public class Main {
-
-    static boolean binarySearch(int [] list,int start, int end, int value){
+    static boolean binarySearch(int [] list, int start, int end, int value){
+        int mid = (start+end)/2;
         if(start>end){
             return false;
         }
-        int mid = (start+end)/2;
         if(list[mid]==value){
             return true;
         }
         else if(value<list[mid]){
             return binarySearch(list,start,mid-1,value);
         }
-        else{
-            return binarySearch(list,mid+1,end,value);
+        else {
+            return binarySearch(list, mid + 1, end, value);
         }
     }
     public static void main(String[] args) {
-        
-
-            try{
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                int user = Integer.parseInt(br.readLine());
-                String [] list_s = new String[user];
-                int [] list = new int[user];
-                list_s = br.readLine().split(" ");
-                for(int i=0;i<user;i++){
-                    list[i]=Integer.parseInt(list_s[i]);
-                }
-                //리스트 오름차순 정렬
-                Arrays.sort(list);
-                int num = Integer.parseInt(br.readLine());
-                String [] list_c = new String[num];
-                list_c = br.readLine().split(" ");
-                for(int i=0;i<num;i++){
-                    if(binarySearch(list,0,user-1,Integer.parseInt(list_c[i]))==true){
-                        list_c[i]="1";
-                    }
-                    else{
-                        list_c[i]="0";
-                    }
-                }
-                for(int i=0;i<num;i++){
-                    System.out.print(list_c[i]+" ");
-                }
-                
-            }catch(IOException e){
-                e.printStackTrace();
-                System.out.println(e.getMessage());
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int num = Integer.parseInt(br.readLine());
+            String [] list_s = new String[num];
+            int [] list = new int[num];
+            list_s = br.readLine().split(" ");
+            for(int i=0;i<num; i++){
+                list[i]= Integer.parseInt(list_s[i]);
             }
+            Arrays.sort(list);
+            num = Integer.parseInt(br.readLine());
+            String [] output = new String[num];
+            output = br.readLine().split(" ");
+            for(int i=0; i<num;i++){
+                if(binarySearch(list,0,list.length-1,Integer.parseInt(output[i]))==true){
+                    output[i]="1";
+                }
+                else{
+                    output[i]="0";
+                }
+            }
+            for(int i=0; i<num;i++){
+                System.out.print(output[i]+" ");
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
+    }
 }
