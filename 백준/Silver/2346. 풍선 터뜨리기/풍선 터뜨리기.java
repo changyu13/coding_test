7baf9a22ff3d16e0d94631995ade8d1d94ca32cf
changyu13 <102674319+changyu13@users.noreply.github.com>
2025-03-14 16:19:34 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 192 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -1,53 +1,54 @@
-import java.io.*;
-import java.util.LinkedList;
-import java.util.Deque;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-public class Main{
-    public static void main(String args[]){
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
         try{
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                int num = Integer.parseInt(br.readLine());
-                String s ="1 ";
-                Deque<int [] > deque = new ArrayDeque<>();
-                String input = br.readLine();
-                int position = 0;
-                 int balloon = 0;
-                if(num==1){
-                    System.out.println(1);
-                }
-                else{                  
-                    for(int i=0; i<num-1;i++){
-                        int idx = input.substring(position,input.length()).indexOf(" ")+position;
-                        balloon = Integer.parseInt(input.substring(position,idx));
-                        deque.addLast(new int[] {(i+1),balloon});
-                        position = idx+1;
-                     }
-                    balloon =  Integer.parseInt(input.substring(position,input.length()));
-                    deque.addLast(new int[] {(num),balloon}); 
-                    int[] first = deque.removeFirst();
-                    while(!deque.isEmpty()){
-                        if(first[1]>0)
-                        {
-                            for(int a=1; a< first[1];a++){
-                                deque.addLast(deque.pollFirst());   
-                            }
-                           first= deque.poll();
-                           s += first[0]+" ";
-                        }
-                        else if(first[1]<0){
-                            for(int a=-1; a> first[1];a--){
-                                deque.addFirst(deque.pollLast());   
-                            }
-                           first= deque.pollLast();
-                           s += first[0]+" ";
-                        }
-                    }
-                    System.out.println(s);    
-                }
-            }catch(IOException e){
-                e.printStackTrace();
-                e.getMessage();
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int num = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Deque<Balloon> queue = new ArrayDeque<>();
+            String answer="";
+            answer += "1 ";
+            int moveValue = Integer.parseInt(st.nextToken());
+            for(int i=2; i<=num; i++){
+                queue.add(new Balloon(i,Integer.parseInt(st.nextToken())));
             }
+
+            while(!queue.isEmpty()){
+                if(moveValue>0){
+                    for(int i=1; i<moveValue; i++){
+                        queue.add(queue.poll());
+                    }
+                    Balloon next = queue.poll();
+                    answer +=next.idx+" ";
+                    moveValue = next.value;
+                }else{
+                    for(int i=1; i<-moveValue;i++){
+                        queue.offerFirst(queue.pollLast());
+                    }
+                    Balloon next = queue.pollLast();
+                    answer += next.idx+" ";
+                    moveValue = next.value;
+                }
+            }
+            System.out.println(answer);
+        }catch(IOException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+    public static class Balloon{
+        int idx;
+        int value;
+
+        public Balloon(int idx, int value){
+            this.idx = idx;
+            this.value = value;
+        }
     }
 }
